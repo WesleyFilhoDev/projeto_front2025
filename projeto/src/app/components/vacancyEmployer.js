d924a1dc8c8c052ccdfa3d862dcb43bd310da51f
odaniellevi <daniellevi.unicap@gmail.com>
2025-06-07 15:11:59 -0300

    commit de controle

--- projeto/src/app/components/vacancyEmployer.js
+++ projeto/src/app/components/vacancyEmployer.js
@@ -1,4 +1,20 @@
 "use client";
 import Image from "next/image";
+import VacancyCard from "./vacancyCard";
 
-export default function vacancyEmployer() {}
+export default function vacancyEmployer() {
+  const vacancies = [
+    {
+      id: 1,
+      title: "Barista",
+      location: "Espinheiro, Recife",
+      image: "/barista.png",
+    },
+    {
+      id: 2,
+      title: "Atendente",
+      location: "Boa Viagem, Recife",
+      image: "/atendente.png",
+    },
+  ];
+}
