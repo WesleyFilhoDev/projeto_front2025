4c018271a0a008cdb1de3742eeb8814de03a1904
odaniellevi <daniellevi.unicap@gmail.com>
2025-06-07 15:11:45 -0300

    primeiros códigos de vacancyEmployer

---
+++ projeto/src/app/testeD/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import VacancyEmployer from "../components/vacancyEmployer";
+
+export default function Home() {
+  return (
+    <>
+      <VacancyEmployer />
+    </>
+  );
+}
